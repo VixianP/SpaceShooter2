d12f03ea4906c91c4f2e1f9f1b062594eee04f1c
vixian <vixian@protonmail.com>
2022-06-02 22:25:30 -0700

    Player Bounds With initial laser prefab

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField]
+    private int speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime,Input.GetAxis("Vertical") * Time.deltaTime, 0);
+
+        Movement();
+    }
+    void Movement()
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        if(transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        } else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -5, 5),0);
+        transform.Translate(h, v, 0);
     }
 }
